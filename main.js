6ea4ab3b4a55d834369194ec8f5d792ab9c7f73d
Vivek prasad <prasadvivek343@gmail.com>
2026-03-17 23:51:47 +0530

    added some comments

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 console.log("Hello, World!");
+// This is a simple JavaScript file that logs "Hello, World!" to the console.
+// You can run this code in a browser's developer console or in a Node.js environment.
