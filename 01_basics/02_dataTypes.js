569883f7357e837626091c92508db99a12b49434
Vivek prasad <prasadvivek343@gmail.com>
2026-03-21 22:02:04 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+  "user strict"; // treat alln JS code as newer version
+
+  // alert (3+3); console.log("vivek")
+
+  console.log(3 
+    +3) // code readability should be high
+
+
+    console.log("vivek") 
+
+     let name="vivek"
+     let age = 18
+     let isloggedIn= false
+     let state ;
+
+     // number=> 2 to power 53
+     //bigint
+     // string=>""
+     // boolean=> true/false
+     //null=>standalone value
+     //undefine =>
+     //symbol=> unique
+
+
+     //object
+
+      console.log(typeof null);//object
+     console.log(typeof undefined);//undefined
+
+
+
