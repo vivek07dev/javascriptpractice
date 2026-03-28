aeb08daaf9bf11884f5c7c5410ea1b499dd51017
Vivek prasad <prasadvivek343@gmail.com>
2026-03-28 21:04:41 +0530

    04_comparision.js

--- 01_basics/03_ conversionOperation.js
+++ 01_basics/03_ conversionOperation.js
@@ -26,6 +26,31 @@ let valueInNumber =Number(score)
   let someNumber = 33
     let stringNumber =String(someNumber)
 
-    console. log(stringNumber);
-    console.log(typeof stringNumber);
-    
+    //console. log(stringNumber);
+    //console.log(typeof stringNumber);
+ 
+    //********************** Operation************************
+
+
+
+    let value=3
+    let negValue =-value
+    //console.log(negValue)
+
+    //console.log(2+2);
+    //console.log(2-2);
+    //console.log(2*2);
+    //console.log(2**2);
+    //console.log(2/3);
+    //console.log(2%3);
+
+
+    let str1 = "hello"
+    let str2 = "vivek"
+
+    let str3 = str1 + str2
+    console.log(str3);
+
+    console.log("1"+2);
+    console.log(1+"2");
+    console.log("1"+2+2);
