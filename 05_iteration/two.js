d7a3eecabe29c26ac6114333607f8d42c1168027
Vivek prasad <prasadvivek343@gmail.com>
2026-04-20 16:30:02 +0530

    two.js

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+
+let index = 0
+//while(index <= 10) {
+    //index = index + 2
+//}
+
+let myArray = ['vivek', "batman", "superman"]
+
+let arr = 0
+while ( arr < myArray.lenght) {
+     // console.log(`value is ${myArray[arr]}`);
+      arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while(score<= 10);
