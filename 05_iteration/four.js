d96c8633b3b69b458a5a7d962cae3510a7ee498c
Vivek prasad <prasadvivek343@gmail.com>
2026-05-24 12:01:37 +0530

    four.js

---
+++ 05_iteration/four.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+
+}
+
+for (const key in myObject){
+    console.log(`${key} shortcut is for 4{myObject[key]}`);
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+
+}
