febf7272b861d3c12c9c076d0a970bb61eb3c32a
Vivek prasad <prasadvivek343@gmail.com>
2026-06-06 14:50:32 +0530

    five.js

---
+++ 05_iteration/five.js
@@ -0,0 +1,39 @@
+const coding = ["js","rb","py","java","cpp"]
+
+//coding.forEach( function (item) {
+    //console.log(item);
+//})
+
+/*coding.forEach( (item) => {
+    console.log(item);
+})*/
+
+//  function printMe(item) {
+    //  console.log(item);
+//  }
+
+//  coding.forEach(printMe)
+
+coding.forEach ((item, index, arr)=> {
+    console.log(item,index,arr);
+})
+
+const  mycodding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+    
+]
+
+mycodding.forEach((item) => {
+    console.log(item.languageName);
+})
